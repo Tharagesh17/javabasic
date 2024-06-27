3374c6f91e6f202289c0814360cdd673186cc1db
Tharagesh17 <98820877+Tharagesh17@users.noreply.github.com>
2024-06-27 11:48:05 +0530

    java basics day2

--- jday2/src/jday2/sample.java
+++ jday2/src/jday2/sample.java
@@ -1,10 +1,20 @@
 package jday2;
 
 public class sample {
+	
 
-	public sample() {
-		// TODO Auto-generated constructor stub
-		System.out.println("hello world");
+	public static void main(String[] args) {
+		char var1 = 'a';
+		System.out.println(var1);
+		char var2 = 65;
+		System.out.println(var2);
+		char var3 = '\u00a7';
+		System.out.println(var3);
+		char var4 = '\u20AC';
+		System.out.println(var4);
+		int a = 'A';
+		System.out.println(a);
+
 	}
 
 }
